77239a6f24565237ae4758527de123de39031261
Aristine-ig <aristine277@gmail.com>
2025-08-28 14:10:21 +0530

    hero

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import MenuIcon from "@/assets/menu.svg"
 export const Header = () => {
   return (
-    <header className='sticky top-0'>
+    <header className='sticky top-0 backdrop-blur-sm'>
       <div className="flex justify-center items-center py-3 bg-[#1c1c1c] text-white text-sm gap-3">
         <p className='text-white/60 hidden md:block'>Streamline your workflow and boost your productivity</p>
         <div className='inline-flex gap-1 items-center'>
@@ -17,7 +17,10 @@ export const Header = () => {
       <div className='py-5'>
         <div className='container'>
           <div className='flex items-center justify-between'>
-            <Image alt='Saas Logo' src={Logo} height={40} width={40} />
+            <div className='flex items-center'>
+              <Image alt='Saas Logo' src={Logo} height={40} width={40} />
+              <div className="font-bold text-3xl px-3 tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text">Chatblizz</div>
+            </div>
             <MenuIcon className="h-5 w-5 md:hidden" />
             <nav className='hidden md:flex gap-6 text-black/60 items-center'>
               <a href='#'>Home</a>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,29 +1,34 @@
 import ArrowIcon from '@/assets/arrow-right.svg'
 import cogImage from '@/assets/cog.png'
+import cylinderImage from '@/assets/cylinder.png'
 import Image from 'next/image';
 
 
 export const Hero = () => {
   return (
-    <section>
+    <section className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183ec2,#eaeefe_66%)]'>
       <div className="container">
-        <div>
-          <div>
-            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Chatblizz is here</div>
-            <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-5">Pathway to Productivity</h1>
-            <p className="text-xl text-[#010d3e] tracking-tight mt-5">Celebrate the joy of accomplishment with an app designed to automate your task&apos;s, motivate your efforts and celebrate your successes.</p>
+        <div className='md:flex items-center'>
+          {/* remove items-center for keeping both in same line */}
+          <div className='md:w-[478px]'>
+            <div>
+              {/* <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Chatblizz is here</div> */}
+              <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-5">Pathway to Productivity</h1>
+              <p className="text-xl text-[#010d3e] tracking-tight mt-5">Celebrate the joy of accomplishment with an app designed to automate your task&apos;s, motivate your efforts and celebrate your successes.</p>
+            </div>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn More</span>
+                <ArrowIcon className="h-5 w-5" />
+              </button>
+            </div>
           </div>
-          <div className="flex gap-1 items-center mt-[30px]">
-            <button className="btn btn-primary">Get for free</button>
-            <button className="btn btn-text gap-1">
-              <span>Learn More</span>
-              <ArrowIcon className="h-5 w-5" />
-            </button>
-          </div>
-        </div>
 
-        <div>
-          <Image src={cogImage} alt='Cog Image' className='h-25 w-20' />
+          <div className='mt-20 md:mt-0 md:h-[648px] flex-1 relative'>
+            <Image src={cogImage} alt='Cog Image' className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6' />
+            <Image src={cylinderImage} alt='Cylinder Image' width={220} height={220} className='hidden md:block -top-8 -left-32 md:absolute' />
+          </div>
         </div>
       </div>
     </section>
