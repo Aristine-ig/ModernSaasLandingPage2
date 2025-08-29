bedc5271982ec0bd5b5ecf528b3f11d8ee27f718
Aristine-ig <aristine277@gmail.com>
2025-08-29 16:23:41 +0530

    pricing

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,5 @@
+import CheckIcon from '@/assets/check.svg';
+
 const pricingTiers = [
   {
     title: "Free",
@@ -51,5 +53,49 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h2 className="section-title py-2">Pricing</h2>
+        <p className="section-description mt-2">
+          Free For Limited period. Upgrade for unlimited tasks, better security,
+          and exclusive features.
+        </p>
+        <div>
+          {pricingTiers.map(
+            ({
+              title,
+              monthlyPrice,
+              buttonText,
+              popular,
+              inverse,
+              features,
+            }, index) => (
+              <div key={index} className='p-10 border border-[#f1f1f1] rounded-3xl shadow-[0_7px_14px_#eaeaea]'>
+                <div className='flex justify-between'>
+                  <h3 className='text-lg font-bold text-black/50'>{title}</h3>
+                  <div className='inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20'>
+                    <span className='bg-[linear-gradient(to_right,#dd7ddf,#e1cd86,#bbcb92,#71c2ef,#3bffff,#dd7ddf)] text-transparent bg-clip-text font-medium'>Popular</span>
+                  </div>
+                </div>
+                <div className='flex items-baseline gap-1 mt-[30px]'>
+                  <span className='text-4xl font-bold tracking-tighter leading-none'>${monthlyPrice}</span>
+                  <span className='tracking-tight font-bold text-black/50'>/month</span>
+                </div>
+                <button className='btn btn-primary w-full mt-[30px]'>{buttonText}</button>
+                <ul className='flex flex-col gap-5 mt-8'>
+                  {features.map((feature, featureIndex) => (
+                    <li key={featureIndex} className='text-sm flex items-center gap-4'>
+                      <CheckIcon className="h-6 w-6" />
+                      <span>{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,25 +1,27 @@
 import productImage from '@/assets/product-image.png'
 import Image from 'next/image';
 import pyramidImage from '@/assets/pyramid.png';
+import tubeImage from "@/assets/tube.png";
 
 export const ProductShowcase = () => {
   return (
-    <section className='bg-gradient-to-b from-[#ffffff] to-[#d2dcff] py-20'>
+    <section className='bg-gradient-to-b from-[#ffffff] to-[#d2dcff] py-20 overflow-x-clip'>
       <div className="container">
         <div className='max-w-[540px] mx-auto'>
           <div className='flex items-center justify-center'>
             <div className='tag'>Boost your productivity</div>
           </div>
-          <h2 className='text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-5'>
+          <h2 className='section-title mt-5'>
             A more effective way to manage your automations.
           </h2>
-          <p className='text-center text-[19px] leading-[30px] tracking-tight text-[#010d3e] mt-1'>
+          <p className='section-description mt-2'>
             Effortlessly automate anything with our website in just few seconds.
           </p>
         </div>
         <div className='relative'>
-          <Image src={productImage} className='mt-10' alt='Product Image'/>
-          <Image src={pyramidImage} alt='Pyramid Image' className='absolute'/>
+          <Image src={productImage} className='mt-10' alt='Product Image' />
+          <Image src={pyramidImage} alt='Pyramid Image' height={262} width={262} className='hidden md:block absolute -right-36 -top-32' />
+          <Image src={tubeImage} height={248} alt='Tube Image' className='hidden md:block absolute bottom-24 -left-36' />
         </div>
       </div>
     </section>
